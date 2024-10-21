b77e867de95cabd5773345b218c8c11052a4a97e
sidneckel <sidn.ick2015@gmail.com>
2024-10-21 09:48:40 -0300

    Finalizando a API REST de livros.

--- app/urls.py
+++ app/urls.py
@@ -12,14 +12,14 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = DefaultRouter()
 
-from core.views import UserViewSet, CategoriaViewSet, EditoraViewSet, AutorViewSet
+from core.views import UserViewSet, CategoriaViewSet, EditoraViewSet, AutorViewSet, LivroViewSet
 
 router = DefaultRouter()
 router.register(r"users", UserViewSet, basename="users")
 router.register(r"categorias", CategoriaViewSet)
 router.register(r"editoras", EditoraViewSet)
 router.register(r"autores", AutorViewSet)
-
+router.register(r"livros", LivroViewSet)
 
 router.register(r"usuarios", UserViewSet, basename="usuarios")
 
