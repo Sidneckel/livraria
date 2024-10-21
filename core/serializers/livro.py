ad795ab35461f77bbce5fd0266f8bf76dfe23096
sidneckel <sidn.ick2015@gmail.com>
2024-10-21 09:58:01 -0300

    Dois serializadores para Livro

--- core/serializers/livro.py
+++ core/serializers/livro.py
@@ -4,4 +4,10 @@ from core.models import Livro
 class LivroSerializer(serializers.ModelSerializer):
     class Meta:
         model = Livro
-        fields = '__all__'
+        fields = "__all__"
+        depth = 1
+class LivroDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = "__all__"
+        depth = 1
