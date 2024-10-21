12e9aae247284287326a5169ab81153eacfd980c
sidneckel <sidn.ick2015@gmail.com>
2024-10-21 09:33:26 -0300

    Adiciona relacionamento de Livro com Categoria e Editora.

--- core/views/autor.py
+++ core/views/autor.py
@@ -1,6 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from core.models import Autor
 from core.serializers import AutorSerializer
+from core.views.livro import LivroViewSet
 
 class AutorViewSet(ModelViewSet):
     queryset = Autor.objects.all()
