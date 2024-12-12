0b6156c26efb14fa3d92eccc616b103b9f0b8061
sidneckel <sidn.ick2015@gmail.com>
2024-12-12 11:05:40 -0300

    Gravando o preço do livro no item da compra.

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import CharField, ModelSerializer, SerializerMethodField,CurrentUserDefault,HiddenField
+from rest_framework.serializers import CharField, ModelSerializer, SerializerMethodField,CurrentUserDefault,HiddenField,ValidationError
 from core.models import Compra, ItensCompra
 
 
@@ -17,6 +17,18 @@ class ItensCompraCreateUpdateSerializer(ModelSerializer):
     class Meta:
         model = ItensCompra
         fields = ("livro", "quantidade")
+
+    def validate_quantidade(self, quantidade):      # nova linha 
+        if quantidade <= 0:
+            raise ValidationError("A quantidade deve ser maior do que zero.")
+        return quantidade
+    
+    def validate(self, item):
+        if item["quantidade"] > item["livro"].quantidade:
+            raise ValidationError("Quantidade de itens maior do que a quantidade em estoque.")
+        return item    
+
+
 
 class ItensCompraListSerializer(ModelSerializer):
     livro = CharField(source="livro.titulo", read_only=True)
@@ -48,18 +60,21 @@ class CompraCreateUpdateSerializer(ModelSerializer):
         fields = ("usuario", "itens")
 
     def create(self, validated_data):
-        itens_data = validated_data.pop("itens")
+        itens = validated_data.pop("itens")
         compra = Compra.objects.create(**validated_data)
-        for item_data in itens_data:
-            ItensCompra.objects.create(compra=compra, **item_data)
+        for item in itens:
+            item["preco"] = item["livro"].preco # nova linha
+            ItensCompra.objects.create(compra=compra, **item)
         compra.save()
         return compra
     def update(self, compra, validated_data):
-        itens_data = validated_data.pop("itens")
-        if itens_data:
+        itens = validated_data.pop("itens")
+        if itens:
             compra.itens.all().delete()
-            for item_data in itens_data:
-                ItensCompra.objects.create(compra=compra, **item_data)
+            for item in itens:
+                item["preco"] = item["livro"].preco  # nova linha
+                ItensCompra.objects.create(compra=compra, **item)
+        compra.save()
         return super().update(compra, validated_data)
 
 class CompraListSerializer(ModelSerializer):
